ea650ce1d5375220a518f363591292f075a50c07
Harrison Clark <harrison.clark99@icloud.com>
2019-09-26 14:24:17 -0400

    Added models to admin.py and removed redundancies from docker compose

--- app/deploy_project/deploy_app/admin.py
+++ app/deploy_project/deploy_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, Government, Government_Citizens, Enterprise, Enterprise_Employees, Enterprise_Customers
+
+admin.site.register(Person)
+admin.site.register(Government)
+admin.site.register(Government_Citizens)
+admin.site.register(Enterprise)
+admin.site.register(Enterprise_Employees)
+admin.site.register(Enterprise_Customers)
+
