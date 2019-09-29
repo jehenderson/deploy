bc3b62994ee84dd7bb8c6429e1f33173443466c0
James Henderson <james@identyti.io>
2019-09-29 16:28:44 -0400

    Pushing fixes to docker compose file

---
+++ app/deploy_project/start-microservices.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sleep 10
+python manage.py migrate
+python manage.py flush --no-input -v 0
+python manage.py loaddata db.json
+mod_wsgi-express start-server --working-directory /app --reload-on-changes /app/deploy_project/wsgi.py
