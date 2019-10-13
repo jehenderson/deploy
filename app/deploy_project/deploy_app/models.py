d66be8421239c9ec7464827ccf80300b3bd93101
Harrison Clark <harrison.clark99@icloud.com>
2019-10-13 16:32:03 -0400

    removed pycache hopefully

--- app/deploy_project/deploy_app/models.py
+++ app/deploy_project/deploy_app/models.py
@@ -26,7 +26,7 @@ class Person(models.Model):
   date_of_birth = models.DateField()
   def __str__(self):
     return self.first_name+" "+self.last_name
-
+ 
 class Government(models.Model):
   name = models.CharField(max_length=50)
   def __str__(self):
