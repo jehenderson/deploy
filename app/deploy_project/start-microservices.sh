6987101f4406701f5b0cc741ee281ff484e9d458
James Henderson <james@identyti.io>
2019-10-16 16:27:48 -0400

    Add port connection loop to microservices

--- app/deploy_project/start-microservices.sh
+++ app/deploy_project/start-microservices.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sleep 10 && python manage.py makemigrations && python manage.py migrate && python manage.py flush --no-input -v 0 && python manage.py loaddata db.json && mod_wsgi-express start-server --working-directory /app --reload-on-changes /app/deploy_project/wsgi.py
+while ! nc -z web 8080; do echo sleeping; sleep 1; done; echo Connected!; sleep 10 && python manage.py makemigrations && python manage.py migrate && python manage.py flush --no-input -v 0 && python manage.py loaddata db.json && mod_wsgi-express start-server --working-directory /app --reload-on-changes /app/deploy_project/wsgi.py
