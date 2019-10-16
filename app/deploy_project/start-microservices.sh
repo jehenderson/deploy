e1505b993bc6da7cff57be19a23bd20991f812c0
James Henderson <james@identyti.io>
2019-10-16 17:10:47 -0400

    add port to mysql in microservices

--- app/deploy_project/start-microservices.sh
+++ app/deploy_project/start-microservices.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-while ! nc -z mysql; do echo sleeping; sleep 1; done; echo Connected!; sleep 10 && python manage.py makemigrations && python manage.py migrate && python manage.py flush --no-input -v 0 && python manage.py loaddata db.json && mod_wsgi-express start-server --working-directory /app --reload-on-changes /app/deploy_project/wsgi.py
-
+while ! nc -z mysql 3306; do echo sleeping; sleep 1; done; echo Connected!; sleep 10 && python manage.py makemigrations && python manage.py migrate && python manage.py flush --no-input -v 0 && python manage.py loaddata db.json && mod_wsgi-express start-server --working-directory /app --reload-on-changes /app/deploy_project/wsgi.py
