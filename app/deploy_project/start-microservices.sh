23af0e237eb2adc626c4a8cd39fb8fc2d1ff093d
James Henderson <james@identyti.io>
2019-10-16 23:10:09 -0400

    Change docker-compose version to 2.1 to support health check

--- app/deploy_project/start-microservices.sh
+++ app/deploy_project/start-microservices.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python poll_db.py && python manage.py makemigrations && python manage.py migrate && python manage.py flush --no-input -v 0 && python manage.py loaddata db.json && mod_wsgi-express start-server --working-directory /app --reload-on-changes /app/deploy_project/wsgi.py
+sleep 10 && python manage.py makemigrations && python manage.py migrate && python manage.py flush --no-input -v 0 && python manage.py loaddata db.json && mod_wsgi-express start-server --working-directory /app --reload-on-changes /app/deploy_project/wsgi.py
